115cbe51fafa3d709aef1f2b30c8359ab6c804de
rishabharyan9 <168530488+rishabharyan9@users.noreply.github.com>
2024-09-02 15:31:23 +0530

    app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from joblib import load
 st.write(f"OpenAI Version: {openai.__version__}")
 
 # Initialize OpenAI API
-openai.api_key = st.secrets['sk-proj-kR5aXzopJymfyUav9PM6T3BlbkFJvkRmukQRTeVsKVHumPgN']
+openai.api_key = st.secrets["OPENAI_API_KEY"]
 
 # Load the model
 model = load('model.joblib')
